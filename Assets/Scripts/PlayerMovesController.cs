9f8323cc7a449eb6a69b329d3c4ba263d63ec667
Alex Y <blablabla@somemail.com>
2022-12-18 00:24:50 +0300

    Animation added(Idle, left hit, right hit)

--- Assets/Scripts/PlayerMovesController.cs
+++ Assets/Scripts/PlayerMovesController.cs
@@ -23,32 +23,33 @@ public class PlayerMovesController : MonoBehaviour
 
     private void Update() {
         canMove = Physics2D.OverlapCircle(transform.position,checkRadius,whatIsGround);
-        Debug.Log(canMove);
         if (swipe.swipedRight == true && transform.position.x < maxX && canMove)
         {     
             targetPos = new Vector2(maxX,transform.position.y);
             StartCoroutine(AfterMove());
+
+
         }
         if (swipe.swipedLeft == true && transform.position.x > minX && canMove)
         {
             targetPos = new Vector2(minX,transform.position.y);
             StartCoroutine(AfterMove());
+
         }
         transform.position = Vector2.MoveTowards(transform.position, targetPos,speed*Time.deltaTime);
     }
     public IEnumerator AfterMove()
     {
-        new WaitForSeconds(0.13f);
-        new WaitUntil(() => transform.position.x == maxX || transform.position.x == minX);
+        anim.SetTrigger("isMoving");
+        yield return new WaitUntil(() => transform.position.x == maxX || transform.position.x == minX);
         if (transform.position.x == maxX)
-        {
-            anim.SetTrigger("left");
-        }
-         if (transform.position.x == minX)
         {
             anim.SetTrigger("right");
         }
+        if (transform.position.x == minX)
+        {
+            anim.SetTrigger("left");
+        }
         yield return null;
     }
-
 }
