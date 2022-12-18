c130a7c8b2b038b618fce737dc8420e29c57bc82
Alex Y <blablabla@somemail.com>
2022-12-18 22:13:50 +0300

    Impediments are added. Health system is introduced.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator anim;
+    public float health;
+    public int numOfHearts;
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    private void FixedUpdate()
+    {
+        if (health > numOfHearts)
+        {
+            health = numOfHearts;
+        }
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < Mathf.RoundToInt(health))
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+            if (i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Impediment"))
+        {
+            health -= 1;
+            anim.SetTrigger("isHit");
+        }
+    }
+
+}
